9929fdcda9cadd9e5113b059363acc1b760c617e
Zehee <zehee@vip.qq.com>
2026-07-18 19:15:58 +0800

    release: v0.3.1

--- src/version.ts
+++ src/version.ts
@@ -6,4 +6,4 @@
  * during protocol initialization and must match the repository (package)
  * version unless explicitly noted otherwise.
  */
-export const VERSION = '0.3.0';
+export const VERSION = '0.3.1';
